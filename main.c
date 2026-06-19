f49742a62bbf44a6275f6e1acc8622654889131b
Victor <vvfernandesneto@gmail.com>
2026-06-19 14:36:30 -0300

    playable pong

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <gb/gb.h>
 #include <stdlib.h>
+#include <gbdk/platform.h>
+#include <gbdk/metasprites.h>
 #include "MyBg.h"
 #include "BolaSprite.h"
-int spriteX,spriteY;
-int8_t velocityX,velocityY;
+#include "RaqueteSprite.h"
+uint8_t joypadPrevious=0, joypadCurrent=0;
+int spriteBallX,spriteBallY;
+int playerX,playerY;
+int cpuX, cpuY;
+int8_t velocityBallX,velocityBallY;
+int8_t velocityplayerX=0,velocityplayerY=0;
+int8_t velocitycpuX=0,velocitycpuY=0;
+
 
 void main(void)
 {
-
+    OBP0_REG = 0xFC;
+    BGP_REG  = 0xFC;
     DISPLAY_ON;
 
     SHOW_BKG;
@@ -16,47 +26,115 @@ void main(void)
     delay(3000);
     HIDE_BKG;
 
+    SPRITES_8x8;
     SHOW_SPRITES;
+
     set_sprite_data(0,1,BolaSprite);
     set_sprite_tile(0,0);
-    spriteX=80;
-    spriteY=72;
+    spriteBallX=80;
+    spriteBallY=72;
 
     /* 
     Setando a velocidade do sprite randomizada no início da partida
     Ainda vou precisar randomizar após cada reinício de partida 
     */
-    velocityX = -1;
-    velocityY = 1;
+    velocityBallX = -1;
+    velocityBallY = 1;
+
+    set_sprite_data(1,2,RaqueteSprite_tiles);
+    playerX = 8;
+    playerY = 86;
+    cpuX = 164;
+    cpuY = 86;
 
 
     // Loop forever
     while(1) {
-        spriteX += velocityX;
-        spriteY += velocityY;
+        // Lógica da movimentação do player
+        joypadPrevious=joypadCurrent;
+        joypadCurrent=joypad();
+        
+        // Lógica da movimentação do player
+        if(joypadCurrent & J_UP) {
+            velocityplayerY=-1;
+        } else if(joypadCurrent & J_DOWN) {
+            velocityplayerY=1;
+        } else {
+            velocityplayerY=0;
+        }
+        playerY+=velocityplayerY;
+        if(playerY < 32) {
+            playerY = 32;
+        }
+        if(playerY > 144) {
+            playerY = 144;
+        }
 
-        if(spriteY < 4) {
-            spriteY = 4;
-            velocityY = -velocityY; // Inverte a direção vertical
+        // Lógica da movimentação da CPU
+        if (velocityBallX > 0) {
+            if (cpuY < spriteBallY - 3) {
+                cpuY += 1;
+            } 
+            else if (cpuY > spriteBallY + 3) {
+                cpuY -= 1;
         }
-        if(spriteY > 140) {
-            spriteY = 140;
-            velocityY = -velocityY; // Inverte a direção vertical
+            if(cpuY < 32) {
+                cpuY = 32;
+            }
+            if(cpuY > 144) {
+                cpuY = 144;
+    }}
+
+        // Lógica da movimentação da bola
+        spriteBallX += velocityBallX;
+        spriteBallY += velocityBallY;
+
+        // Lógica da colisão da bola com as paredes superior e inferior
+        if(spriteBallY < 16) {
+            spriteBallY = 16;
+            velocityBallY = -velocityBallY; // Inverte a direção vertical
         }
-        if (spriteX < -4 || spriteX > 164) {
-            if (spriteX < -4) {
+        if(spriteBallY > 152) {
+            spriteBallY = 152;
+            velocityBallY = -velocityBallY; // Inverte a direção vertical
+        }
+
+        // Lógica da saída da bola da tela (gol)
+        if (spriteBallX < 0 || spriteBallX > 168) {
+            if (spriteBallX < 0) {
                 // Lógica do gol pro time da direita
             } else {
                 // Lógica do gol pro time da esquerda
             }
-            spriteX = 80;
-            spriteY = 72;
+            spriteBallX = 84;
+            spriteBallY = 84;
+            playerY = 86;
+            cpuY = 86;
             delay(1000); // Pausa pra mostrar o gol
-            move_sprite(0,spriteX+4,spriteY+12); // Reposiciona o sprite antes de esperar para evitar que ele fique invisível
+            move_metasprite_ex(RaqueteSprite_metasprites[0], 1, 0, 1, playerX, playerY);
+            move_metasprite_ex(RaqueteSprite_metasprites[0], 1, 0, 5, cpuX, cpuY);
+            move_sprite(0,spriteBallX,spriteBallY); // Reposiciona o sprite antes de esperar para evitar que ele fique invisível
+            velocityBallX = -1;
             delay(1000); // Pausa pra se preparar
         }
 
-        move_sprite(0,spriteX+4,spriteY+12);
+        // Lógica da colisão da bola com a raquete do player
+        if (velocityBallX < 0 && spriteBallX <= playerX + 2 && spriteBallX >= playerX - 2) { 
+            if (spriteBallY >= (playerY - 20) && spriteBallY <= (playerY + 20)) {
+                velocityBallX = -velocityBallX;
+                spriteBallX = playerX + 2;
+                }}
+
+        // Lógica da colisão da bola com a raquete da CPU
+        if (velocityBallX > 0 && spriteBallX <= cpuX && spriteBallX >= cpuX - 4) { 
+            if (spriteBallY >= (cpuY - 20) && spriteBallY <= (cpuY + 20)) {
+                velocityBallX = -velocityBallX;
+                spriteBallX = cpuX - 4;
+                }}
+
+        move_sprite(0,spriteBallX,spriteBallY);
+        move_metasprite_ex(RaqueteSprite_metasprites[0], 1, 0, 1, playerX, playerY);
+        move_metasprite_ex(RaqueteSprite_metasprites[0], 1, 0, 5, cpuX, cpuY);
 		// Done processing, yield CPU and wait for start of next frame
         wait_vbl_done();
     }
--- make.bat
+++ make.bat
@@ -5,9 +5,10 @@ DEL *.gb
 C:\gbdk\bin\lcc -c -o main.o main.c
 C:\gbdk\bin\lcc -c -o MyBg.o MyBg.c
 C:\gbdk\bin\lcc -c -o BolaSprite.o BolaSprite.c
+C:\gbdk\bin\lcc -c -o RaqueteSprite.o RaqueteSprite.c
 
 :: Compile a .gb file from the compiled .o files
-C:\GBDK\bin\lcc -o Logo.gb main.o MyBg.o BolaSprite.o
+C:\GBDK\bin\lcc -o Jogável.gb main.o MyBg.o BolaSprite.o RaqueteSprite.o
 
 :: delete intermediate files created for the conmpilation process
 DEL *.asm
